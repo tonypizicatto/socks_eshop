eaeea71abf764f8c2b4710a7a0e4ca052437253c
Tony Pizicatto <13diego13@mail.ru>
2021-04-13 11:10:55 +0300

    add show page product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,18 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :destroy, :update]
 
   def index
     @products = Product.all
+  end
+
+  def show
+  end
+
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 
 end
