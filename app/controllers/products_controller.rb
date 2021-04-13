7d4b71bb8112fa9dd5ab00a9b9f90b6f878b2b25
Tony Pizicatto <13diego13@mail.ru>
2021-04-13 12:35:33 +0300

    add sidebar, about us page and bootstrap

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,9 +5,7 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
-  def show
-  end
-
+  def show; end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :products
 
   root "products#index"
+
+  get 'dashboard/about'
 
 end
