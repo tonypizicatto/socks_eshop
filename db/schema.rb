7b81a29e91df0da6052f40badeb68a72a1023408
Tony Pizicatto <13diego13@mail.ru>
2021-04-13 15:42:17 +0300

    add name and phone to user

--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2021_04_13_102215) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "name", null: false
+    t.string "name"
     t.integer "phone"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
